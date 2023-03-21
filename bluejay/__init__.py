493100e201d6bcd8be926a9256f4a841b7b4ff66
Filippo Signorini <f.signorini@elsyco.it>
2023-03-21 17:58:08 +0100

    Updated main module imports

--- bluejay/__init__.py
+++ bluejay/__init__.py
@@ -1,4 +1,20 @@
-from .interfaces.advertisement import Advertisement
-from .interfaces.agent import Agent
-from .interfaces.gatt import Application, Characteristic, Descriptor, Service
-from .managers.ble_manager import BLEManager
+try:
+    from bluejay.interfaces.advertisement import Advertisement
+except ImportError:
+    from .interfaces.advertisement import Advertisement
+
+try:
+    from bluejay.interfaces.agent import Agent
+except ImportError:
+    from .interfaces.agent import Agent
+
+try:
+    from bluejay.interfaces.gatt import Application, Characteristic, Descriptor, Service
+except ImportError:
+    from .interfaces.gatt import Application, Characteristic, Descriptor, Service
+
+try:
+    from bluejay.managers.ble_manager import BLEManager
+except ImportError:
+
+    from .managers.ble_manager import BLEManager
