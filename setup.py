3819e30595a750c8a4f2b04d2453c2b99628019e
Filippo Signorini <f.signorini@elsyco.it>
2023-03-23 12:53:42 +0100

    Updated to version 0.0.5

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="bluejay",
-    version="0.0.4",
+    version="0.0.5",
     author="Filippo Signorini",
     description="A collection of BLE utils",
     long_description=long_description,
