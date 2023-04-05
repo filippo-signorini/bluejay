4ba26a2d6b2dd358241232029bf5b7411db34293
Filippo Signorini <f.signorini@elsyco.it>
2023-04-05 17:09:33 +0200

    Return the timer ID

--- bluejay/glib.py
+++ bluejay/glib.py
@@ -1,21 +1,20 @@
 from typing import Callable
+
 import dbus.mainloop.glib
 from gi.repository import GLib as _GLib  # type: ignore
 
 
 class GLib:
-
     @staticmethod
     def MainLoop():
         return MainLoop()
 
     @staticmethod
-    def timeout_add(interval_milli: int, cb: Callable):
-        _GLib.timeout_add(interval_milli, cb)
+    def timeout_add(interval_milli: int, cb: Callable) -> int:
+        return _GLib.timeout_add(interval_milli, cb)
 
 
 class MainLoop:
-
     def __init__(self):
         dbus.mainloop.glib.DBusGMainLoop(set_as_default=True)
         self._mainloop = _GLib.MainLoop()
